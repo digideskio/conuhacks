f964af77f74614a998c0f3f219e066918f2d12d8
Pascal Chouinard <pascal.chouinard@appdirect.com>
2016-01-06 16:46:30 -0500

    rename script

--- webtasks/create_webtask.sh
+++
@@ -1,4 +0,0 @@
-wt create GetEndpoints.js \
-          --name getendpoints \
-          --secret firebase_url=https://boiling-fire-9252.firebaseio.com/ \
-					--secret firebase_secret=kVOyQvDaFeqIAZj75WVdx77mmboANyWow9qkGU4m
