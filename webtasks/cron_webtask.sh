effb1726556b7c325fb4de27f34c6a9fc640026f
Bruno Carriere <bruno.carriere@appdirect.com>
2016-01-23 11:36:29 -0500

    No more secrets

--- webtasks/cron_webtask.sh
+++ webtasks/cron_webtask.sh
@@ -1,4 +1,4 @@
 wt cron schedule "0 * * * * *" GetEndpoints.js \
           --name getendpoints \
           --secret firebase_url=https://boiling-fire-9252.firebaseio.com/ \
-					--secret firebase_secret=kVOyQvDaFeqIAZj75WVdx77mmboANyWow9qkGU4m
+					--secret firebase_secret=$FIREBASE_SECRET
